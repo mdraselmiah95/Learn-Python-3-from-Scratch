b22e2279edc0ddba8330ea1e535fd2b1246acee6
mdraselmiah95 <russelroy15@gmail.com>
2022-10-26 22:29:20 +0600

    Python input and output function

--- Start-with-Python-3/input-output-function.py
+++ Start-with-Python-3/input-output-function.py
@@ -1,11 +1,28 @@
 # Ask user name and print user name
+'''
 user_name=input("What is your name? ")
-# print("Hello "+user_name)
+print("Hello "+user_name)
+'''
 
 #Ask user age and add 5 with that age
+'''
 user_age=input("What is your age? ")
 age=int(user_age)
-# print(age)
-# print("Your age is", age+5)
+print(age)
+print("Your age is", age+5)
+'''
 
-print(type(input))
+# print(type(input))
+
+'''
+Sep Keyword
+
+print("hello world\n",10)
+print("hello wold","hello python","Goon evening",sep="**")
+print('Hello world',sep="***")
+'''
+
+#End keyword
+print("I love Python",end="***")
+print("THis is my friend",end="\n")
+print("THis is my laptop",end="\n")
