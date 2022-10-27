615d5ffe7c28984468c93ed39c1222fd5804cb52
mdraselmiah95 <russelroy15@gmail.com>
2022-10-27 19:39:08 +0600

    WIP on Comments Escape Sequence

--- Start-with-Python-3/Comments-Escape-Sequence.py
+++ Start-with-Python-3/Comments-Escape-Sequence.py
@@ -3,3 +3,23 @@
 # 1. Single line Comments
 # 2. Multiline line Comments
 # 3. Docstring line Comments 
+
+# Multi
+# line
+# comment
+
+'''
+This is a
+docstring
+comment
+'''
+
+def multiply(a,b):
+    '''Multiplies the value of a and b'''
+    return a*b
+
+result=multiply(3,9)
+print(result)
+
+# Print the docstring of multiply function
+print(multiply.__doc__)    
