ac4c4633a17cbd050124a3629b802369437d1e8c
mdraselmiah95 <russelroy15@gmail.com>
2022-10-27 19:31:21 +0600

    WIP on Comments Escape Sequence

---
+++ Start-with-Python-3/Comments-Escape-Sequence.py
@@ -0,0 +1,5 @@
+# Comments enhance the readability of the code and help the programmers to understand the code very carefully. 
+# There are three types of comment in Python-
+# 1. Single line Comments
+# 2. Multiline line Comments
+# 3. Docstring line Comments 
